612c5d976c491090f889ab67e20eee8b99dc9bf8
龙马行空 <shiguoqing999@163.com>
2015-11-05 11:45:13 +0800

    列表页刷新加载更多完毕

--- www/areas/details/details_route.js
+++ www/areas/details/details_route.js
@@ -2,7 +2,7 @@ angular.module('APT.details.route', ['APT.details.controller'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('details', {
-        url: '/details',
+        url: '/details/:productId',
         templateUrl: 'areas/details/details.html',
         controller: 'DetailsCtrl'
       })
--- www/areas/goodsList/goodsList_controller.js
+++ www/areas/goodsList/goodsList_controller.js
@@ -4,59 +4,96 @@
  功  能：商品列表页面
  ******************************************************/
 angular.module('APT.goodsList.controller', ['APT.goodsList.service'])
-  .controller('GoodsListCtrl', ['$scope', '$ionicHistory','$state','GoodsListFty','CommonJs', function ($scope, $ionicHistory,$state,GoodsListFty,CommonJs) {
+  .controller('GoodsListCtrl', ['$scope', '$ionicHistory','$state','GoodsListFty','CommonJs','$stateParams',
+    function ($scope, $ionicHistory,$state,GoodsListFty,CommonJs,$stateParams) {
 
-    // 数据对象
-    $scope.obj_goodsListData=null;
-    // 有更多数据可以加载
-    $scope.pms_isMoreItemsAvailable = true;
-    // 分页查询对象
-    $scope.obj_pagingInfo = {
-      amountMax: "",
-      amountMin: "",
-      billNum: "",
-      createUser: "",
-      dateFrom: "",
-      dateTo: "",
-      deptID: "",
-      deptName: "",
-      keyWord: "",
-      loginName: "",
-      billType: "",
-      pageNum: "1",
-      pageSize: "10",
-      sortFlag: "0",
-      sortType: "desc"
-    };
+      $scope.$on('$stateChangeSuccess', function(e) {
+        $scope.func_refreshGoodsList();
+      });
+
+      $scope.$on('$ionicView.enter', function (e) {
+
+      });
+
+      // 数据对象
+      $scope.obj_goodsListData=null;
+      // 有更多数据可以加载
+      $scope.pms_isMoreItemsAvailable =true;
+      // 分页查询对象
+      $scope.obj_pagingInfo = {
+        amountMax: "",
+        amountMin: "",
+        billNum: "",
+        createUser: "",
+        dateFrom: "",
+        dateTo: "",
+        deptID: "",
+        deptName: "",
+        keyWord: "",
+        loginName: "",
+        billType: "",
+        pageNum: 1,
+        pageSize: 10,
+        sortFlag: "0",
+        sortType: "desc",
+        typeNumber:""
+      };
 
 
-    $scope.$on('$ionicView.enter', function (e) {
-      $scope.func_refreshGoodsList();
-    });
+      /**
+       * 下拉刷新方法
+       */
+      $scope.func_refreshGoodsList = function () {
+        $scope.obj_pagingInfo.pageNum = 1;
+        $scope.obj_pagingInfo.typeNumber= $stateParams.typeNumber;
+        console.log('刷新'+$scope.obj_pagingInfo.pageNum);
+        var message = JSON.stringify($scope.obj_pagingInfo);
 
+        var promise = GoodsListFty.refreshGoodsList(message);
+        promise.then(
+          function (result) {
+            $scope.obj_goodsListData = result;
+          },
+          function (reason) {
+            CommonJs.AlertPopup("抱歉,请求数据失败！");
+          }
+        ).finally(function () {
+            // 刷新完毕要广播刷新完毕事件
+            $scope.$broadcast('scroll.refreshComplete');
+          });
+      };
 
-    /**
-     * 下拉刷新方法
-     */
-    $scope.func_refreshGoodsList = function () {
-      $scope.obj_pagingInfo.pageNum = "1";
-      var message = JSON.stringify($scope.obj_pagingInfo);
-      // 获取分类信息列表
-      var promise = GoodsListFty.refreshGoodsList(message);
-      promise.then(
-        function (result) {
-          //$scope.obj_goodsListData = JSON.parse(result.resultData);
-          $scope.obj_goodsListData = result;
-          // 加载更多配置3-设置没有更多可以加载
-          //$scope.isMoreItemsAvailable = true;
-        },
-        function (reason) {
-          CommonJs.AlertPopup("抱歉,请求数据失败！");
-        }
-      ).finally(function () {
-          // 刷新完毕要广播刷新完毕事件
-          $scope.$broadcast('scroll.refreshComplete');
-        });
-    };
+      /**
+       * 上拉加载更多方法
+       */
+      $scope.func_loadMoreGoodsList = function () {
 
-  }]);
+        //if($scope.obj_pagingInfo.pageNum==1&&$scope.pms_isMoreItemsAvailable==true) {
+        //  $scope.$broadcast('scroll.infiniteScrollComplete');
+        //}
+        $scope.obj_pagingInfo.pageNum = $scope.obj_pagingInfo.pageNum + 1;
+        $scope.obj_pagingInfo.typeNumber= $stateParams.typeNumber;
+        console.log('加载更多'+$scope.obj_pagingInfo.pageNum);
+        var message = JSON.stringify($scope.obj_pagingInfo);
+
+        var promise = GoodsListFty.loadMoreGoodsList(message);
+        promise.then(
+          function (result) {
+            if(result!=null){
+              $.each(result,function(i,item){
+                $scope.obj_goodsListData.push(item);
+              });
+            }else{
+              $scope.pms_isMoreItemsAvailable=false;
+            }
+          },
+          function (reason) {
+            CommonJs.AlertPopup("抱歉,请求数据失败！");
+          }
+        ).finally(function () {
+            // 加载更多完毕要广播刷新完毕事件
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+          });
+      };
+    }
+  ]);
